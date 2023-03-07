af471d0a6c96a1249461b584cb38b9ef0b12093c
James Nora <125532446+kynokee@users.noreply.github.com>
2023-03-07 04:30:17 +0000

    Updated files as cher did ;D

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return 'Hello from Flask!'
+  data = None # Ensures error is not triggered
+  numbers = [1, 2, 3, 4, 5]
+  with open('intro.txt', 'r') as file:
+    msg = file.read()
+  if request.method == 'POST':
+    data = request.form['name']
+    with open('secret.txt', 'a') as file:
+      file.write(data + '\n')
+  return render_template('index.html', name=data, nums=numbers, intro=msg)
 
 
 app.run(host='0.0.0.0', port=81)
