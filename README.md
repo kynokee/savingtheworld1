3edc9a6d3ab8405778007150e7a431bab254c945
James Nora <125532446+kynokee@users.noreply.github.com>
2023-03-18 11:34:51 +0000

    Finalised webapp

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Webapp
 
-This is my first savingtheworld app which
+This is my first savingtheworld app which lets me get to know the user better
 
 Replit: https://replit.com/@kynokee/webapp
 
--- main.py
+++ main.py
@@ -5,15 +5,25 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-  data = None # Ensures error is not triggered
-  numbers = [1, 2, 3, 4, 5]
+  data = None
+  name = None
+  gender = None
+  Class = None
+  H2 = None
+  H1 = None
   with open('intro.txt', 'r') as file:
     msg = file.read()
   if request.method == 'POST':
-    data = request.form['name']
+    name = request.form.get('name')
+    Class = request.form.get('class')
+    gender = request.form.get('gender')
+    H2 = request.form.getlist('H2')
+    H1 = request.form.get('H1')
+    data = name + ' ' + Class + ' ' + gender + ' H1: ' + H1 + ' H2: ' 
+    for sub in H2:
+      data = data + sub + ' '
     with open('secret.txt', 'a') as file:
       file.write(data + '\n')
-  return render_template('index.html', name=data, nums=numbers, intro=msg)
-
+  return render_template('index.html', intro=msg, egg=Class)
 
 app.run(host='0.0.0.0', port=81)
